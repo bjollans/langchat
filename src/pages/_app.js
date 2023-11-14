98e56e5170e34576488eb4c03f4e0d2d422a3e4b
Bernard Jollans <jollans94@googlemail.com>
2023-11-14 15:24:43 +0530

    connect analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "styles/global.css";
 import "util/analytics";
+import { PHProvider } from 'util/providers'
 import { AuthProvider } from "util/auth";
 import { QueryClientProvider } from "util/db";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <QueryClientProvider>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </QueryClientProvider>
+    <PHProvider>
+      <QueryClientProvider>
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </QueryClientProvider>
+    </PHProvider>
   );
 }
 

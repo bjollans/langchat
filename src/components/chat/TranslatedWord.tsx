f5f96bc41823f8c667e2a003024fdbb417825f6a
Bernard Jollans <jollans94@googlemail.com>
2023-11-18 22:56:16 +0530

    Render word translations in app

---
+++ src/components/chat/TranslatedWord.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export interface TranslatedTermProps {
+    term: string;
+    translation: string | undefined;
+}
+
+export default function TranslatedTerm(props: TranslatedTermProps): JSX.Element {
+    const [showTranslation, setShowTranslation] = useState(false);
+
+    return (
+        <span className="relative cursor-pointer"
+            onClick={() => setShowTranslation(true)}
+            onMouseLeave={() => setShowTranslation(false)}>
+            <div className={showTranslation ? "cursor-text absolute bottom-0 left-0" : "hidden"}>
+                <div className="bg-black whitespace-nowrap text-white rounded-lg p-2 mb-6">
+                    {props.translation}
+                </div>
+            </div>
+            <span className="mx-0.5 underline decoration-dotted hover:text-blue-500 cursor-pointer">
+                {props.term}
+            </span>
+        </span>
+    )
+}
--- src/model/message.ts
+++ src/model/message.ts
@@ -1,8 +1,20 @@
 import { Language } from "../types/language";
 
+export interface TermTranslation {
+    term: string;
+    translation: string;
+    position: number;
+}
+
+export interface TranslationJson {
+    wholeSentence: string;
+    terms: Array<TermTranslation>;
+}
+
 export interface Message {
     role: "user" | "assistant";
     content: string;
     createdAt: Date;
     conversationId: string;
+    translationJson?: TranslationJson;
 }

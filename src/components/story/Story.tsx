c254e34b277d44366fd52805e1d25570983c2c07
Bernard Jollans <jollans94@googlemail.com>
2023-12-01 17:45:35 +0100

    Fix image size above story

--- src/components/story/Story.tsx
+++ src/components/story/Story.tsx
@@ -34,7 +34,7 @@ export default function Story(props: StoryProps): JSX.Element {
     return (
         <div className="flex">
             <div className={`p-4 my-4 rounded-lg border-1 border-black`}>
-                <img className="h-96 w-1/2 mx-auto object-cover rounded-lg shadow-md shadow-black flex-none" src={story?.imageUrl} alt="" />
+                <img className="h-96 lg:w-1/2 w-full md:w-2/3 mx-auto object-cover rounded-lg shadow-md shadow-black flex-none" src={story?.imageUrl} alt="" />
                 <div className="border-b border-gray-200 pb-5 my-8 flex items-end">
                     <h3 className="mx-6 text-base text-4xl mx-auto font-semibold leading-6 text-gray-900">{story?.title}</h3>
                 </div>

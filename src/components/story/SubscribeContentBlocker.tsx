38706d7bfc95ae2a2a7cd768108549f093acadca
Bernard Jollans <jollans94@googlemail.com>
2023-12-19 21:48:15 +0100

    Improve readability of subscription blocker popup

--- src/components/story/SubscribeContentBlocker.tsx
+++ src/components/story/SubscribeContentBlocker.tsx
@@ -2,7 +2,7 @@ import PricingSection from "components/PricingSection";
 
 export function SubscribeContentBlocker() {
     return (
-        <div className="flex justify-center absolute w-full z-10 h-full backdrop-blur-sm">
+        <div className="flex justify-center absolute w-full z-10 h-full backdrop-blur-sm bg-white bg-opacity-60">
             <div className="rounded-lg p-4 mx-auto">
                 <h3 className="text-black text-4xl max-w-lg tracking-tight text-center font-bold mb-4">
                     You have reached your free weekly limit of 3 stories

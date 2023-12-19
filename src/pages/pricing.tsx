60ef5d2b246e4cee26a380719a094ee6b17dc282
Bernard Jollans <jollans94@googlemail.com>
2023-12-19 19:17:12 +0100

    Extract PricingSection from PricingPage

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,4 +1,5 @@
 import { CheckIcon } from '@heroicons/react/20/solid'
+import PricingSection from 'components/PricingSection';
 
 const subscriptionDetails =
 {
@@ -24,44 +25,7 @@ export default function PricingPage() {
           Get Access!
         </p>
       </div>
-      <div className="mx-auto mt-8 items-center max-w-sm">
-        <div
-          key={"subscriptionDetails"}
-          className={classNames(
-            'relative bg-white shadow-2xl',
-            'rounded-3xl ring-1 ring-gray-900/10 p-10'
-          )}
-        >
-          <p className="mt-4 flex items-baseline gap-x-2">
-            <span className="text-5xl font-bold tracking-tight text-gray-900">{subscriptionDetails.priceMonthly}</span>
-            <span className="text-base text-gray-500">/month</span>
-          </p>
-          <p className="mt-6 text-base leading-7 text-gray-600">
-            Get access to all stories and articles.
-          </p>
-          <p className="text-lg leading-7 text-gray-600">
-            We add <b>new content every week</b>.
-          </p>
-          <ul role="list" className="space-y-3 text-sm leading-6 text-gray-600 mt-10">
-            {subscriptionDetails.features.map((feature) => (
-              <li key={feature} className="flex gap-x-3">
-                <CheckIcon className="h-6 w-5 flex-none text-cyan-600" aria-hidden="true" />
-                {feature}
-              </li>
-            ))}
-          </ul>
-          <a
-            href={subscriptionDetails.href}
-            aria-describedby={"subscriptionDetails"}
-            className={classNames(
-              'bg-cyan-600/90 text-white shadow hover:bg-cyan-500',
-              'mt-10 block rounded-md py-2.5 px-3.5 text-center text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600'
-            )}
-          >
-            Try 7 days for free
-          </a>
-        </div>
-      </div>
+      <PricingSection />
     </div>
   )
 }

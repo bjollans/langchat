a2db3ef1826c60f414e938ca1e1a3a85887c8591
Bernard Jollans <jollans94@googlemail.com>
2024-01-06 23:06:56 +0100

    Duplicate story overview on index page with canonical link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,27 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import posthog from "posthog-js";
+import Meta from "components/Meta";
+import StoryList from "components/story/StoryList";
 
 function IndexPage() {
-  const { push, query } = useRouter();
+  const { query } = useRouter();
   const hasPaid = query.paid;
 
   useEffect(() => {
     if (hasPaid == 'true') {
       posthog.capture('purchase');
     }
-    if (query) {
-      push('/story/hi');
-    }
   }, [query]);
-  return <p></p>;
+
+  return <>
+    <Meta title="Hindi Mini Stories" alternated={{canonical: `${process.env.NEXT_PUBLIC_SITE_URL}/story/hi`}} />
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-12">
+      <div className="mx-auto max-w-3xl">
+        <StoryList />
+      </div>
+    </div>
+  </>
 }
 
 export default IndexPage;

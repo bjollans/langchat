53c69350dc0a75bddbcc0cba68bcbd652a73f045
Bernard Jollans <jollans94@googlemail.com>
2024-01-06 23:16:17 +0100

    Debugging failed sitemap build

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,9 @@ module.exports = {
     additionalPaths: async (config) => {
         // get urls from "url" field in json returned from "https://linguin.co/api/get-story-pages"
         const stories = await fetch("https://linguin.co/api/get-story-pages");
+        console.log("stories: ", stories);
         const storiesJson = await stories.json();
+        console.log("storiesJson: ", storiesJson);
         const storyUrls = storiesJson.map((story) => story.url);
         return storyUrls;
     }

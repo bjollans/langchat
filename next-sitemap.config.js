9d4aa2f2e647c104a65121b83f248bdc6489a557
Bernard Jollans <jollans94@googlemail.com>
2024-01-06 23:19:32 +0100

    Remove debug statements and AI commands from code

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,11 +3,8 @@ module.exports = {
     generateRobotsTxt: true,
     exclude: ["https://www.linguin.co/chat"],
     additionalPaths: async (config) => {
-        // get urls from "url" field in json returned from "https://linguin.co/api/get-story-pages"
         const stories = await fetch("https://linguin.co/api/get-story-pages");
-        console.log("stories: ", stories);
         const storiesJson = await stories.json();
-        console.log("storiesJson: ", storiesJson);
         const storyUrls = storiesJson.urls;
         return storyUrls;
     }

5e0d37098f221ae3487120de51a7ae832df2ff90
Bernard Jollans <jollans94@googlemail.com>
2024-01-06 23:38:57 +0100

    Add story specific meta description and title

--- src/pages/story/hi/[id].tsx
+++ src/pages/story/hi/[id].tsx
@@ -2,13 +2,21 @@ import Meta from "components/Meta";
 import Story from "components/story/Story";
 import { StoryIdContext } from "context/storyIdContext";
 import { useRouter } from "next/router";
+import { useStory, useStoryCollections } from "util/db";
 
 function StoryPage() {
   const router = useRouter();
   const { id } = router.query;
+  const { data: story } = useStory(id as string);
+  const { data: collections } = useStoryCollections(id as string);
+
+  const collectionsAsStrings = (collections as any)?.map((c) => c.collectionName).join(", ");
 
   return <>
-    <Meta title="Story for Hindi Reading Practice" />
+    <Meta title={`Hindi Reading Practice - ${story?.title}`} 
+    description={`${story?.difficulty} story for Hindi reading practice and listening practice. 
+    Improve your Hindi with reading practice with Hindi passages. 
+    This one has these categories: ${collectionsAsStrings}`}/>
 
     <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
       <div className="mx-auto">

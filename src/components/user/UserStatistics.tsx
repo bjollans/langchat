557bfcad86dfee4db2c0cec25f16fbb194911ff7
Bernard Jollans <jollans94@googlemail.com>
2024-01-12 22:58:42 +0100

    Attempt 2 to fix wordsSeen bug

--- src/components/user/UserStatistics.tsx
+++ src/components/user/UserStatistics.tsx
@@ -5,7 +5,7 @@ export default function UserStatistics() {
     const auth = useAuth();
     const { data: wordsSeenJson } = userWordsSeen(auth?.user?.uid);
     const { data: storiesRead } = useUserStoriesRead(auth?.user?.uid);
-    const wordsSeen = wordsSeenJson?.length > 0 ? wordsSeenJson[0]?.wordsSeen : [];
+    const wordsSeen = wordsSeenJson?.length > 0 && wordsSeenJson[0] ? wordsSeenJson[0]?.wordsSeen : [];
 
     return <p className="mx-auto text-center mb-2 mt-1 text-xs italic leading-6 font-medium text-gray-900">
         You have read {storiesRead?.length ?? 0} stories and seen {wordsSeen?.length} words.

257cfb86a1ace2bfcefbae194cdb1f7358db5b13
Bernard Jollans <jollans94@googlemail.com>
2024-01-16 11:49:22 +0100

    Fix import error

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,6 +5,7 @@ import { PHProvider } from 'util/providers'
 import { AuthProvider } from "util/auth";
 import { QueryClientProvider } from "util/db";
 import ApplicationShell from "components/ApplicationShell";
+import Script from 'next/script';
 
 
 function MyApp({ Component, pageProps }) {

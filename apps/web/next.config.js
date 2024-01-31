023408a132543ff446183d4cdab8720f774d4a9d
Bernard Jollans <jollans94@googlemail.com>
2024-01-31 09:04:06 +0100

    Next App works again

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,16 +1,17 @@
 const { withExpo } = require('@expo/next-adapter');
 
-module.exports = withExpo({
-  // transpilePackages is a Next.js +13.1 feature.
-  // older versions can use next-transpile-modules
-  transpilePackages: [
-    'react-native',
-    'expo',
-    'nativewind',
-    "react-native-css-interop",
-    // Add more React Native/Expo packages here...
-  ],
-  experimental: {
-    forceSwcTransforms: true,
-  },
+const nextConfig = withExpo({
+    reactStrictMode: true,
+    transpilePackages: ['linguin-shared',
+        'react-native',
+        'react-native-web',
+        'nativewind',
+        "react-native-css-interop",
+        // Add more React Native/Expo packages here...
+    ],
+    experimental: {
+        forceSwcTransforms: true,
+    },
 });
+
+module.exports = nextConfig

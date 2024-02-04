5870131c4da6f2cc596ab10034b775b0927546fb
Bernard Jollans <jollans94@googlemail.com>
2024-02-04 15:51:11 +0100

    Revert "Move most code to shared library"
    This reverts commit 2550c54d99438d0b3cf14685fa25dd957a9d6456.

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "../../packages/linguin-shared/components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},

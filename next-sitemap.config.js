fc7f213964f22634f8d435a19489f5865926ee75
Bernard Jollans <jollans94@googlemail.com>
2024-02-09 18:50:44 +0530

    Delete unused pages and components as part of 'app' directory migration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     siteUrl: 'https://www.linguin.co',
     generateRobotsTxt: true,
-    exclude: ["/chat", "/auth/*", "/settings/*", "/vocab", "/vocab/*", "/practice", "/articles/cheat-sheet"]
+    exclude: ["/auth/*", "/settings/*", "/articles/cheat-sheet"]
 }

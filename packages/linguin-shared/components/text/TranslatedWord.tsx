8743afb9751b0ad3d240bdfce4a7770c31d09494
Bernard Jollans <jollans94@googlemail.com>
2024-02-15 18:05:46 +0530

    Move hovering translation box into own file

--- packages/linguin-shared/components/text/TranslatedWord.tsx
+++ packages/linguin-shared/components/text/TranslatedWord.tsx
@@ -1,10 +1,10 @@
-import { Div, P, Span, Btn, Br } from "linguin-shared/components/RnTwComponents";
-import WordPlayerButton from "linguin-shared/components/audio/WordPlayerButton";
+import { Btn, Span } from "linguin-shared/components/RnTwComponents";
 import { StoryIdContext } from "linguin-shared/context/storyIdContext";
 import { useReadUsageContext } from "linguin-shared/context/trackReadContext";
 import { TermTranslation } from "linguin-shared/model/translations";
 import posthog from "posthog-js";
 import { useContext, useState } from "react";
+import TranslatedWordHoverBox from "./TranslatedWordHoverBox";
 
 export interface TranslatedTermProps {
     termTranslation: TermTranslation;
@@ -30,20 +30,7 @@ export default function TranslatedTerm(props: TranslatedTermProps): JSX.Element 
             onClick={handleClick}
             onMouseLeave={() => setShowTranslation(false)}
             className="cursor-pointer relative mx-0.5 underline decoration-dotted hover:text-indigo-500 cursor-pointer">
-            {showTranslation && <Div>
-                <Div className="flex flex-row space-x-1 bg-black absolute bottom-0 left-0 rounded-lg p-3 mb-6 mx-auto -space-y-2 items-center">
-                    <WordPlayerButton word={props.termTranslation.text} />
-                    <Div className="flex flex-col items-start">
-                        <P className="text-white align-start text-lg ">{props.termTranslation.translation}</P>
-                        {props.termTranslation.transliteration &&
-                            <P className="align-start text-white text-sm italic">
-                                {props.termTranslation.transliteration}
-                            </P>
-                        }
-                    </Div>
-
-                </Div>
-            </Div>}
+            {showTranslation && <TranslatedWordHoverBox termTranslation={props.termTranslation} />}
             <Span>
                 {props.termTranslation.text}
             </Span>
---
+++ packages/linguin-shared/components/text/TranslatedWordHoverBox.tsx
@@ -0,0 +1,25 @@
+import { Div, P } from "linguin-shared/components/RnTwComponents";
+import WordPlayerButton from "linguin-shared/components/audio/WordPlayerButton";
+import { TermTranslation } from "linguin-shared/model/translations";
+
+export interface TranslatedTermProps {
+    termTranslation: TermTranslation;
+}
+
+export default function TranslatedWordHoverBox(props: TranslatedTermProps): JSX.Element {
+    return (<Div>
+        <Div className="flex flex-row space-x-1 bg-black absolute bottom-0 left-0 rounded-lg p-3 mb-6 mx-auto -space-y-2 items-center">
+            <WordPlayerButton word={props.termTranslation.text} />
+            <Div className="flex flex-col items-start">
+                <P className="text-white align-start text-lg ">{props.termTranslation.translation}</P>
+                {props.termTranslation.transliteration &&
+                    <P className="align-start text-white text-sm italic">
+                        {props.termTranslation.transliteration}
+                    </P>
+                }
+            </Div>
+
+        </Div>
+    </Div>
+    );
+}

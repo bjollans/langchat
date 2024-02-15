3c3cea9c539bfed209ce0c920378e74a7345a937
Bernard Jollans <jollans94@googlemail.com>
2024-02-15 17:05:33 +0530

    Add playable word sound to RN

--- packages/linguin-shared/components/audio/WordPlayerButton.tsx
+++ packages/linguin-shared/components/audio/WordPlayerButton.tsx
@@ -4,6 +4,7 @@ import { useContext, useMemo, useRef } from "react";
 import { Platform} from "react-native";
 import { Btn, Div } from "linguin-shared/components/RnTwComponents";
 import { PlayCircleIcon } from "linguin-shared/components/Icons";
+import Sound from 'react-native-sound';
 
 export interface WordPlayerButtonProps {
     word: string;
@@ -20,13 +21,27 @@ export default function WordPlayerButton({ word }: WordPlayerButtonProps) {
         return `https://backend.linguin.co/storage/v1/object/public/wordSound/${fileName}.mp3`;
     }, [word]);
 
+    const rnSound = useMemo(() => {
+        return new Sound(audioSrc, '', (error) => {
+            if (error) {
+                console.log('failed to load the sound', error);
+                return;
+            }
+        });
+    }, [audioSrc]);
+
     const play = () => {
-        if (audioRef.current) {
-            audioRef.current.play();
-            posthog.capture("play_word_sound", {
-                vocab: word,
-                storyId: storyId,
-            });
+        console.log("PLAYING AUDIO!!!");
+        if (Platform.OS == "web") {
+            if (audioRef.current) {
+                audioRef.current.play();
+                posthog.capture("play_word_sound", {
+                    vocab: word,
+                    storyId: storyId,
+                });
+            }
+        } else {
+            rnSound.play();
         }
     };
 

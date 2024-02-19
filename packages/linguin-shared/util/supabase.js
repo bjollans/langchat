153fd1aec979a7cf8c11a14165cbd918eda263ba
Bernard Jollans <jollans94@googlemail.com>
2024-02-19 10:50:56 +0530

    Separate supabase client options for RN

--- packages/linguin-shared/util/supabase.js
+++ packages/linguin-shared/util/supabase.js
@@ -1,8 +1,24 @@
 import { createClient } from "@supabase/supabase-js";
+import { Platform } from "react-native";
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.EXPO_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY || process.env.EXPO_PUBLIC_SUPABASE_PUBLIC_KEY,
-);
+
+const supabase = Platform.OS == "web"
+  ? createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY,
+  )
+  : createClient(
+    process.env.EXPO_PUBLIC_SUPABASE_URL,
+    process.env.EXPO_PUBLIC_SUPABASE_PUBLIC_KEY,
+    {
+      auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+      }
+    }
+  );
 
 export default supabase;

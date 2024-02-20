e7d585b04374757190569a05ab10ecac047d0a15
Bernard Jollans <jollans94@googlemail.com>
2024-02-20 20:07:48 +0530

    Make auth work on all platforms again

--- packages/linguin-shared/util/supabase.js
+++ packages/linguin-shared/util/supabase.js
@@ -1,31 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { AppState, Platform } from "react-native";
 
-const supabase = Platform.OS == "web" ? createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY,
-) : createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL,
-  process.env.EXPO_PUBLIC_SUPABASE_PUBLIC_KEY,
-  {
-    auth: {
-      storage: AsyncStorage,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-    }
-  }
-);
-
-if (Platform.OS != "web") {
-  AppState.addEventListener('change', (state) => {
-    if (state === 'active') {
-      supabase.auth.startAutoRefresh()
-    } else {
-      supabase.auth.stopAutoRefresh()
-    }
-  });
-}
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY,
+  );
 
 export default supabase;

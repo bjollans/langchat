351eab3b508b4339dd32b8d079e8b16040d31936
Bernard Jollans <jollans94@googlemail.com>
2024-02-22 12:46:39 +0530

    Add contact page

---
+++ apps/web/src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import { BugAntIcon, ChatBubbleLeftRightIcon, ComputerDesktopIcon } from '@heroicons/react/24/outline'
+
+export default function Contact() {
+  return (
+    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl sm:text-center">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Support</h2>
+        <p className="mt-2 text-lg leading-8 text-gray-600">
+          If you need help with anything, contact: bernard [at] linguin.co
+        </p>
+      </div>
+    </div>
+  )
+}

e60b5e4eae64ad3552aaf8b841eac9a87624d941
Bernard Jollans <jollans94@googlemail.com>
2024-03-02 18:16:42 +0700

    Fix title and description in hindi story overview

--- apps/web/src/app/story/hi/page.tsx
+++ apps/web/src/app/story/hi/page.tsx
@@ -6,6 +6,7 @@ import UserStatistics from "linguin-shared/components/user/UserStatistics";
 import { StoryText, StoryToCollection } from "model/translations";
 import { getAvailableStoryDifficultyLevels, getCollectionNames, getStoriesCollections, getStoriesOrderedByCustom } from "util/serverDb";
 import StoryListFilterContextProvider from "@linguin-shared/context/storyListFilterContext";
+import { Metadata } from "next/types";
 
 async function getPropsForStoryIndexPage() {
     const stories = await getStoriesOrderedByCustom('title', false);
@@ -31,7 +32,6 @@ async function StoryIndexPage() {
     const propsForStoryIndexPage = await getPropsForStoryIndexPage();
     return (
         <>
-            <Meta title="Linguin - Hindi Reading Practice" />
             <PurchasedTracker />
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="mx-auto max-w-3xl">
@@ -44,6 +44,11 @@ async function StoryIndexPage() {
             </div>
         </>
     );
+}
+
+export const metadata: Metadata = {
+    title: 'Linguin - Hindi Reading Practice',
+    description: 'Improve your Hindi reading! Practice with Stories, Indian Myths and Informational Texts. Instant translations and pronounciation guides. All texts with audio narration. ',
 }
 
 export default StoryIndexPage;

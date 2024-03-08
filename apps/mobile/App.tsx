54436e293ab4fb2ff460f2bb38f00264a7a496a0
Bernard Jollans <jollans94@googlemail.com>
2024-03-08 11:03:19 +0700

    Add account screen to unsubscribe and sign out

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from 'linguin-shared/util/auth';
 import { QueryClientProvider } from 'linguin-shared/util/clientDb';
 import supabase from 'linguin-shared/util/supabase';
 import { useEffect, useState } from 'react';
-import { Platform } from 'react-native';
+import { Platform, Touchable, TouchableOpacity } from 'react-native';
 import mobileAds, { MaxAdContentRating } from 'react-native-google-mobile-ads';
 import Purchases from 'react-native-purchases';
 import 'react-native-url-polyfill/auto';
@@ -15,11 +15,14 @@ import AuthForm from './components/AuthForm';
 import StoryListScreen from './screens/StoryListScreen';
 import StoryPaywallScreen from './screens/StoryPaywallScreen';
 import Story from './screens/StoryScreen';
+import AccountScreen from './screens/AccountScreen';
+import Svg, { Path } from 'react-native-svg';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [session, setSession] = useState<Session | null>(null)
 
-  const Stack = createNativeStackNavigator();
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -76,9 +79,11 @@ export default function App() {
                 }
               }}>
                 <Stack.Screen name="StoryList" component={StoryListScreen}
-                  options={({ route }) => ({
+                  options={({ route, navigation }) => ({
                     title: "🇮🇳 Hindi Stories",
-                    headerRight: (route.params as any)?.filterButton
+                    headerTitleAlign: 'center',
+                    headerRight: (route.params as any)?.filterButton,
+                    headerLeft: () => <TouchableOpacity onPress={() => navigation.navigate('Account')}><_AccountIcon /></TouchableOpacity>
                   })} />
                 <Stack.Screen name="StoryPaywall" component={StoryPaywallScreen}
                   options={({ route }) => ({
@@ -90,6 +95,10 @@ export default function App() {
                     title: (route.params as any).storyTitle,
                     headerBackTitle: "Stories"
                   })} />
+                <Stack.Screen name="Account" component={AccountScreen}
+                  options={() => ({
+                    title: "Account"
+                  })} />
               </Stack.Navigator>
             </NavigationContainer>
           </StoriesAvailableContextProvider>
@@ -97,4 +106,9 @@ export default function App() {
       </AuthProvider>
     </QueryClientProvider>
   );
+}
+
+
+const _AccountIcon = () => {
+  return (<Svg height="24" viewBox="0 -960 960 960" width="24"><Path fill="#64748b" d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/></Svg>);
 }

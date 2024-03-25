43a1c08221ff54d5b93ac1e14ff899ba47b94fe5
Bernard Jollans <jollans94@googlemail.com>
2024-03-25 16:51:41 +0700

    Add Sentry Crash and Error Reporting

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -20,10 +20,16 @@ import StoryListScreen from './screens/StoryListScreen';
 import StoryPaywallScreen from './screens/StoryPaywallScreen';
 import Story from './screens/StoryScreen';
 import { initNotifications, scheduleReminderNotification } from './util/notifications';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://4a146848a62a8271f335ecdcc6ea9c5a@o4505409086095360.ingest.us.sentry.io/4506970903805952',
+  debug: false,
+});
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+function App() {
 
   const [session, setSession] = useState<Session | null>(null)
 
@@ -125,3 +131,6 @@ export default function App() {
 const _AccountIcon = () => {
   return (<Svg height="24" viewBox="0 -960 960 960" width="24"><Path fill="#64748b" d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" /></Svg>);
 }
+
+
+export default Sentry.wrap(App);

7bdb775c2856dbe50fe1661e6383d98ba687a359
Bernard Jollans <berjollans@gmail.com>
2024-05-29 17:46:37 +0200

    Include app store link on landing page

--- apps/web/src/components/landing/Hero.tsx
+++ apps/web/src/components/landing/Hero.tsx
@@ -18,19 +18,23 @@ export default function Hero() {
                     <p className="mt-6 text-lg leading-8 text-gray-900">
                         Practice Reading Hindi with our Hindi text passages.
                     </p>
-                    <div className="mt-10 flex gap-x-6 items-center max-lg:justify-center">
-                        <div className="w-60">
-                            <a onClick={() => posthog.capture("android_hero_link_click", {via: viaCookie})}
-                            href='https://play.google.com/store/apps/details?id=com.bjolly.linguin&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' /></a>
+                    <div className="lg:flex items-center">
+                        <div className="mt-10 flex max-lg:justify-center">
+                            <a onClick={() => posthog.capture("ios_hero_link_click", { via: viaCookie })}
+                                href='https://apps.apple.com/de/app/linguin-hindi-reading/id6478230990?l=en-GB'
+                            >
+                                <img className=" h-[52px] mt-[13px] my-[23px]" alt='Download on the App Store' src='/images/app-store-download.svg' />
+                            </a>
+                            <a onClick={() => posthog.capture("android_hero_link_click", { via: viaCookie })}
+                                href='https://play.google.com/store/apps/details?id=com.bjolly.linguin&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img className="h-[78px] max-lg:mx-auto" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' /></a>
                         </div>
                         <a
-                            onClick={() => posthog.capture("web_hero_link_click", {via: viaCookie})}  
+                            onClick={() => posthog.capture("web_hero_link_click", { via: viaCookie })}
                             href="/story/hi"
-                            className="rounded-md h-fit bg-blue-900 px-3.5 py-3.5 sm:text-2xl text-md font-semibold shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 text-white focus-visible:outline-offset-2 focus-visible:outline-amber-400 mr-4"
+                            className="rounded-lg h-[60px] lg:h-[52px] mt-[30px] max-lg:mx-auto lg:mx-[13px] bg-slate-900 text-slate-100 px-[12px] py-[11px] font-semibold text-lg"
                         >
                             Read in Browser
-                        </a>
-                    </div>
+                        </a></div>
                 </div>
             </div>
             <img className="z-10 h-5/6 max-sm:max-w-xs max-lg:max-w-lg mx-auto items-center my-auto ml-0 mr-auto max-lg:mx-auto" src="/images/phone.png" />
